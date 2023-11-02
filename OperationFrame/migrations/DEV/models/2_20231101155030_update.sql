a3f9b7abedfcd071c011f44d7c53157ca209721b
lihengling <1309399041@qq.com>
2023-11-02 11:12:11 +0800

    忽略sql

--- OperationFrame/migrations/DEV/models/2_20231101155030_update.sql
+++
@@ -1,4 +0,0 @@
--- upgrade --
-ALTER TABLE `permissions` DROP COLUMN `parent_id`;
--- downgrade --
-ALTER TABLE `permissions` ADD `parent_id` INT NOT NULL  COMMENT '父id' DEFAULT 0;
--- OperationFrame/migrations/DEV/models/3_20231102110144_update.sql
+++
@@ -1,6 +0,0 @@
--- upgrade --
-ALTER TABLE `menu` ADD `parent_id` INT   COMMENT '父菜单关系外键';
-ALTER TABLE `menu` ADD CONSTRAINT `fk_menu_menu_a0892170` FOREIGN KEY (`parent_id`) REFERENCES `menu` (`id`) ON DELETE CASCADE;
--- downgrade --
-ALTER TABLE `menu` DROP FOREIGN KEY `fk_menu_menu_a0892170`;
-ALTER TABLE `menu` DROP COLUMN `parent_id`;
