1a3524e1282933dd135bffcebbc8b4cb54b96b99
lihengling <1309399041@qq.com>
2023-11-02 11:24:46 +0800

    fixbug

--- OperationFrame/utils/aerich.py
+++ OperationFrame/utils/aerich.py
@@ -3,12 +3,15 @@
 Author: 'LingLing'
 Date: 2023/03/24
 """
+import os
 from aerich import Command
 
 from OperationFrame.config import config
 
 
-aer = Command(
-    tortoise_config=config.MYSQL_TORTOISE_ORM,
-    location=f"./{config.FRAME_NAME}/migrations/{config.ENV}",
-)
+location = f"./{config.FRAME_NAME}/migrations/{config.ENV}"
+
+if not os.path.exists(location):
+    os.makedirs(location)
+
+aer = Command(tortoise_config=config.MYSQL_TORTOISE_ORM, location=location)
