5bd17f0bdceb485507bbbf03ba2ee66b016a0a57
lihengling <1309399041@qq.com>
2024-03-06 15:42:36 +0800

    完成基础接口

--- OperationFrame/ApiFrame/apps/index/view.py
+++ OperationFrame/ApiFrame/apps/index/view.py
@@ -3,28 +3,3 @@
 Author: 'LingLing'
 Date: 2023/03/03
 """
-from typing import Union, List, Any
-from pydantic import BaseModel
-from typing_extensions import Annotated
-
-from fastapi import Query, Path, BackgroundTasks
-
-from OperationFrame.ApiFrame.base import router_index
-
-
-class UserIn(BaseModel):
-    username: str
-    password: str
-    email: str
-    full_name: Union[str, None] = None
-
-
-class UserOut(BaseModel):
-    username: str
-    email: str
-    full_name: Union[str, None] = None
-
-
-@router_index.post("/user11/", response_model=UserOut)
-async def create_user(user: UserIn) -> Any:
-    return user
